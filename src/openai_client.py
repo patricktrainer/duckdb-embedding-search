c971e4f8a1786777be408f272e705c5c87dfd1aa
Patrick Trainer <pat.trainer@gmail.com>
2024-01-11 11:43:57 -0600

    move embedding creation to client

--- src/openai_client.py
+++ src/openai_client.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
+from typing import List
 from openai import OpenAI
-
 
 load_dotenv()
 
@@ -11,3 +11,16 @@ def get_openai_client() -> OpenAI:
     key = os.getenv("OPENAI_API_KEY")
     client = OpenAI(api_key=key)
     return client
+
+def create_embedding(text: str, model: str = "text-embedding-ada-002", **kwargs) -> List[float]:
+    # replace newlines, which can negatively affect performance.
+    try: 
+        client = get_openai_client()
+    except Exception as e:
+        print(e)
+        return []
+    
+    text = text.replace("\n", " ")
+    response = client.embeddings.create(input=[text], model=model, **kwargs)
+    return response.data[0].embedding
+
