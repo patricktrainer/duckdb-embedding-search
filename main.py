0d76208e13114cb7759b8284a75dc06317076b6f
Patrick Trainer <pat.trainer@gmail.com>
2024-01-11 13:42:59 -0600

    manage the connection context

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from src.connection import open_connection
 from src.embedding import get_similarity
 
 if __name__ == "__main__":
-    # existing main functionality
-    con = open_connection("embeddings")
     model = "text-embedding-ada-002"
-
     test_text = """
         One thing I’ve noticed is that many engineers, when they’re looking for a library on Github, they check the last commit time. They think that the more recent the last commit is, the better supported the library is.
         But what about an archived project that does exactly what you need it to do, has 0 bugs, and has been stable for years? That’s like finding a hidden gem in a thrift store!
         Most engineers I see nowadays will automatically discard a library that is not "constantly" updated... Implying it's a good thing :)
         """
 
-    for result in get_similarity(con, test_text, model):
-        print(
-            f"""
-            text: {result[0]}
-            similarity: {result[1]}
-            """
-        )
+    with open_connection("hn_embeddings") as con:
+        for result in get_similarity(con, test_text, model):
+            print(
+                f"""
+                text: {result[0]}
+                similarity: {result[1]}
+                """
+            )
+
+   
+
